bc7ec8ea86569d3406a778509b9b4340bd1e147f
Anastasiiiii <nastia262002@gmail.com>
2023-06-13 22:17:16 +0300

    correct mistakes with bootstrap styles and images

--- server.js
+++ server.js
@@ -1,17 +1,37 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static(__dirname + '/node_modules'));
+
+
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 
 app.get('/home', (req, res) => {
-    res.sendFile(__dirname + '/src/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/css/styles.css', (req, res) => {
-    res.sendFile(__dirname + '/src/css/styles.css');
+app.get('/style.css', (req, res) => {
+    res.sendFile(__dirname + '/style.css');
 });
+
+app.get('/modal-styles.css', (req, res) => {
+    res.sendFile(__dirname + '/modal-styles.css');
+});
+
+app.get('/node_modules/bootstrap/dist/css/bootstrap.min.css', (req, res) => {
+    res.sendFile(__dirname + '/node_modules/bootstrap/dist/css/bootstrap.min.css');
+});
+
+app.get('/node_modules/bootstrap-social/bootstrap-social.css', (req, res) => {
+    res.sendFile(__dirname + '/node_modules/bootstrap-social/bootstrap-social.css');
+});
+
+app.get('/node_modules/bootstrap/dist/css/bootstrap.min.css.map', (req, res) => {
+    res.sendFile(__dirname + '/node_modules/bootstrap/dist/css/bootstrap.min.css.map');
+});
+
 
 app.listen(3000, () => {
     console.log('Server running at http://localhost:3000/');
