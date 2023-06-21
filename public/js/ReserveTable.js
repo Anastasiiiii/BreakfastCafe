9923f6d7eddc9cf88e10d286e9c11487c69db8e9
Anastasiiiii <nastia262002@gmail.com>
2023-06-21 19:32:30 +0300

    add info from the reserved a table modal in the local storage

--- public/js/ReserveTable.js
+++ public/js/ReserveTable.js
@@ -1,9 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
-const reserveTableButton = document.getElementById("reserveTableButton");
-const closeModalButton = document.getElementById("closeModalButton");
-const cancelReserveButton = document.getElementById("cancelReserveButton");
+    const reserveTableButton = document.getElementById("reserveTableButton");
+    const closeModalButton = document.getElementById("closeModalButton");
+    const cancelReserveButton = document.getElementById("cancelReserveButton");
 
-const reserveModal = new bootstrap.Modal(document.getElementById("reserveModal"));
+    const reserveModal = new bootstrap.Modal(document.getElementById("reserveModal"));
+
     reserveTableButton.addEventListener("click", function() {
         reserveModal.show();
     });
@@ -16,5 +17,41 @@ const reserveModal = new bootstrap.Modal(document.getElementById("reserveModal")
         reserveModal.hide();
     });
 
-    loginModal.show();
+    //loginModal.show();
 });
+
+window.onload = function() {
+    //Choose guests-number
+    document.getElementById('btn-for-reservation').addEventListener("click", function(event) {
+    event.preventDefault();
+  
+    const guestsNumber = document.querySelector('.guestsnum:checked').value;
+    localStorage.setItem('guestsNum', guestsNumber);
+  
+    // Choose guest-location
+    const terraceOption = document.getElementById("terrace-location");
+    const indoorOption = document.getElementById("indoor-location");
+
+    let terraceOptionValue;
+    terraceOption.onclick = function setTerraceOption() {
+        terraceOptionValue = "terrace location";
+        localStorage.setItem('guestsLocation', terraceOptionValue);
+    }
+
+    let indoorOptionValue
+    indoorOption.onclick = function setIndoorOption() {
+        indoorOptionValue = "indoor location";
+        localStorage.setItem('guestsLocation', indoorOptionValue);
+    }
+
+    // Choose date and time
+    const timeValue = document.getElementById("time").value;
+    const dateValue = document.getElementById("data").value;
+    const dateTime = {
+      time: timeValue,
+      date: dateValue
+    };
+
+    localStorage.setItem('guestsDateTime', JSON.stringify(dateTime));
+  });
+}
--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,27 @@
+document.addEventListener('DOMContentLoaded', function() {
+    showComments();
+    function showComments() {
+        let commentField = document.getElementById('comment-field');
+        let out = '';
+        comments.forEach(function(item, index) {
+            out += `<div class="alert alert-primary alert-dismissible fade show" role="alert">
+                        <div class="d-flex justify-content-between align-items-center">
+                            <p class="mb-0"><b>${item.name}</b></p>
+                            <p class="mb-0 text-right small"><em>${timeConverter(item.time)}</em></p>
+                        </div>
+                        <button type="button" class="close delete-button" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                        <p>${item.feedback}</p>
+                    </div>`;
+        });
+        commentField.innerHTML = out;
+        deleteComments();
+    }
+});
+
+
+
 if (typeof window !== 'undefined') {
     window.onload = function() {
         var input = document.getElementById("input");
